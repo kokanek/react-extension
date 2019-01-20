be5b3a174eac8006ba171708f6ee68aec79bdd73
kokanek <kapeel.kokane@gmail.com>
2019-01-20 14:24:21 +0530

    settings up react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import { Switch, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/settings' component={Settings}/>
+    </Switch>
+  </main>
+)
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Link to='/settings/1'><Button variant="contained" color="primary">Add Pages</Button></Link>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -9,19 +29,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button variant="contained" color="primary">Test Button</Button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Main/>
         </header>
+      </div>
+    );
+  }
+}
+
+class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <p className="text">This is the settings component</p>
       </div>
     );
   }
