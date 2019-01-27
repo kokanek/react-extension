3cee71112df378e321adc55534c1c2b5e7a53f2b
kokanek <kapeel.kokane@gmail.com>
2019-01-27 17:36:05 +0530

    supported delete

--- src/Settings.js
+++ src/Settings.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
 
 
@@ -11,6 +12,8 @@ export default class Settings extends Component {
       this.handleOnClick = this.handleOnClick.bind(this);
       this.updateSites = this.updateSites.bind(this);
       this.handleOnChange = this.handleOnChange.bind(this);
+      this.handleOnDelete = this.handleOnDelete.bind(this);
+      
 
       this.state = {
           sites: [],
@@ -19,7 +22,7 @@ export default class Settings extends Component {
     }
 
     componentDidMount() {
-        this.updateSites(this.props.match.params.number)
+        this.updateSites(this.props.match.params.number);
     }
   
     handleOnClick(e) {
@@ -29,11 +32,12 @@ export default class Settings extends Component {
         chrome.storage.sync.get([key], function(result) {
             console.log('got the result: ', result);
             siteArray = result[key] || [];
-            siteArray.push({siteName: that.state.currentSite});
+            const id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 9);;
+            siteArray.push({siteName: that.state.currentSite, id: id});
             chrome.storage.sync.set({[key]: siteArray}, function() {
                 console.log('Value is set:', key, siteArray);
             });
-            that.setState({sites: siteArray})
+            that.setState({sites: siteArray, currentSite: ''})
         });   
     }
 
@@ -41,9 +45,28 @@ export default class Settings extends Component {
         this.setState({currentSite: e.target.value})
     }
   
+    handleOnDelete(e) {
+        var removeByAttr = function(arr, attr, value){
+            var i = arr.length - 1;
+            while(i >= 0){
+                if( arr[i] && arr[i][attr] == value ){ 
+                    arr.splice(i,1);
+                }
+
+                i--;
+            }
+            return arr;
+        }
+
+        let newSitesArr = removeByAttr(this.state.sites, 'id', e.currentTarget.id);
+        this.setState({sites: newSitesArr})
+        chrome.storage.sync.set({[this.props.match.params.number]: newSitesArr}, function() {
+            console.log('Value is set after delete');
+        });
+    }
+
     render() {
-        console.log(this.props.match.params.number);
-        let sites = this.state.sites.map((site) => <h3>{site && site.siteName}</h3>);
+        let sites = this.state.sites.map((site) => <h3>{site && site.siteName}<Button onClick={this.handleOnDelete} id={site.id || null}><DeleteIcon/></Button></h3>);
         return (
             <div>
                 <div className="row-format">
@@ -65,9 +88,12 @@ export default class Settings extends Component {
 
     updateSites(key) {
         let siteArray = [];
+        let that = this;
         chrome.storage.sync.get([key], function(result) {
-            siteArray = result.key;
-            this.setState({sites: siteArray});
+            siteArray = result[key];
+            that.setState({sites: siteArray});
+            return;
         });
+        this.setState({sites: []})
     }
 }
